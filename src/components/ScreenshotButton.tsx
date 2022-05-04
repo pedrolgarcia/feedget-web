4aa2164bb2c14c80b566efd1188e6da4e5401273
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-05-04 19:10:29 -0300

    feat: take screen shot, fake send form

--- src/components/ScreenshotButton.tsx
+++ src/components/ScreenshotButton.tsx
@@ -1,7 +1,46 @@
-import { Camera } from "phosphor-react";
+import { useState } from "react";
+import html2canvas from "html2canvas";
+import { Camera, Trash } from "phosphor-react";
+import { Loading } from "./Loading";
 
-export function ScreenshotButton() {
-  function handleTakeScreensho() {}
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton({
+  screenshot,
+  onScreenshotTook
+}: ScreenshotButtonProps) {
+  const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
+
+  async function handleTakeScreensho() {
+    setIsTakingScreenshot(true);
+
+    const canvas = await html2canvas(document.querySelector("html")!);
+    const base64Image = canvas.toDataURL("image/png");
+
+    onScreenshotTook(base64Image);
+
+    setIsTakingScreenshot(false);
+  }
+
+  if (screenshot) {
+    return (
+      <button
+        type="button"
+        className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        onClick={() => onScreenshotTook(null)}
+        style={{
+          backgroundImage: `url(${screenshot})`,
+          backgroundPosition: "right bottom",
+          backgroundSize: 100
+        }}
+      >
+        <Trash weight="fill" />
+      </button>
+    );
+  }
 
   return (
     <button
@@ -9,7 +48,11 @@ export function ScreenshotButton() {
       onClick={handleTakeScreensho}
       className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
-      <Camera className="w-6 h-6 text-zinc-100" />
+      {isTakingScreenshot ? (
+        <Loading />
+      ) : (
+        <Camera className="w-6 h-6 text-zinc-100" />
+      )}
     </button>
   );
 }
